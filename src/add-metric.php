3c2bc01cf4b2ab1d2570b9364eef64190597d1f3
ramon-wang-rw <wangramon1@gmail.com>
2022-04-25 19:41:15 -0400

    your mom

--- src/add-metric.php
+++ src/add-metric.php
@@ -2,14 +2,16 @@
 
 include 'config.php';
 
-if (isset($_POST['weight']) && isset($_POST['height']) && isset($_POST['name'])){
+if (isset($_POST['weight']) && isset($_POST['height']) && isset($_POST['name']) && isset($_POST['gender']) && isset($_POST['age'])){
     $weight = $_POST['weight'];
     $height = $_POST['height'];
     $name = $_POST['name'];
+    $gender = $_POST['gender'];
+    $age = $_POST['age'];
     if ($name != "" && $height != 0 && $weight != 0){
         $executeFirst = "DELETE FROM metric";
         mysqli_query($conn, $executeFirst);
-        $sql = "INSERT INTO metric (name, weight,height) VALUES ('$name', '$weight','$height')";
+        $sql = "INSERT INTO metric (name, weight,height, gender, age) VALUES ('$name', '$weight','$height', '$gender', '$age')";
         $result = mysqli_query($conn, $sql);
         if ($result) {
             echo 1;
--- src/show-metric.php
+++ src/show-metric.php
@@ -9,8 +9,23 @@ if (mysqli_num_rows($result) > 0) {
         $n = $row['name'];
         $w = $row['weight'];
         $h = $row['height'];
+        $g = $row['gender'];
+        $a = $row['age'];
     }
 }
 
-echo '<div class="total">Hi, ' . $n . ', your height is ' . $h . ' cm and your weight is ' . $w . ' lbs.</div>';
+$wk = $w * 0.45359237;
+if($g == "Male") {
+    $mc = ((88.4 + 13.4 * $wk) + (4.8 * $h) - (5.68 * $a)) * 1.375;
+    $mc = round($mc);
+}
+
+else{
+    $mc = ((447.6 + 9.25 * $wk) + (3.1 * $h) - (4.33 * $a)) * 1.375;
+    $mc = round($mc);
+}
+
+echo '<div class="total">Hi, ' . $n . ', your height is ' . $h . ' cm and your weight is ' . $w . ' lbs.</div>
+        <br>
+        <div class="mc">Your recommended daily calories is ' .$mc. ' calories';
 ?>
